5c16187f657f5cc23bb0b98061367a091b6cc196
Frez <csfrez@163.com>
2021-11-27 14:29:39 +0800

    示例更新

--- src/test/java/com/csfrez/http/OKHttpClientTest.java
+++ src/test/java/com/csfrez/http/OKHttpClientTest.java
@@ -43,8 +43,18 @@ public class OKHttpClientTest {
     public void testIntercept() throws Exception{
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(chain -> {
             Request request = chain.request();
-            //System.out.println(request.headers());
-            return chain.proceed(request);
+
+            long t1 = System.nanoTime();
+            System.out.println(String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+            System.out.println(String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
         }).build();
         String url = "https://www.baidu.com";
         Request request = new Request.Builder()
